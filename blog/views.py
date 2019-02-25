ec343050250a51024c31b28fa7825c01b32340e6
Vlad Repin <vladyslav.riepin@gmail.com>
2019-02-25 18:18:22 +0200

    Sixth commit.

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'RepinV',
-        'title': 'Post1',
-        'content': 'First post content',
-        'date_posted': 'August 26, 2018',
-    },
-    {
-        'author': 'RepinD',
-        'title': 'Post2',
-        'content': 'Second post content',
-        'date_posted': 'April 27, 2018',
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
